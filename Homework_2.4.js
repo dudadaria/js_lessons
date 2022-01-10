88ef7fc6924daa5708dd3db8414b6f6bc6d9d8b5
dariaduda <96076657+dariaduda@users.noreply.github.com>
2022-01-10 22:52:42 +0300

    Homework_2.3 + 2.4

---
+++ Homework_2.4.js
@@ -0,0 +1,28 @@
+"use strict"
+
+let a = null;
+let b = null;
+
+function sum (a, b){
+  return (a+b)
+}
+
+console.log(sum(2,4))
+
+function multi (a, b){
+  return (a*b)
+}
+
+console.log(multi(2,4))
+
+function div (a, b){
+  return (a/b)
+}
+
+console.log(div(2,4))
+
+function diff (a, b){
+  return (a-b)
+}
+
+console.log(diff(2,4))
