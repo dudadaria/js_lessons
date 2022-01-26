e19715b1e810c3063b44cd680363763af7c1c4ce
dariaduda <96076657+dariaduda@users.noreply.github.com>
2022-01-26 19:02:40 +0300

    Homework_4 (add game_walking)

---
+++ game.js
@@ -0,0 +1,29 @@
+"use strict";
+
+let game = {
+  /**
+   * Запускает игру
+   */
+  run: function () {
+    while (true) {
+      //Получаем направление от игрока.
+      const direction = mover.getDirection();
+      if (direction === null) {
+        console.log('Игра окончена');
+        return;
+      }
+      const nextPoint = mover.getNextPosition(direction);
+      renderer.clear();
+      player.move(nextPoint);
+      renderer.render();
+    }
+  },
+  //Этот метод выполняется при открытии страницы
+  init() {
+    console.log('Ваше положение на поле в виде --> о');
+    renderer.render();
+    console.log("Чтобы начать игру наберите game.run() и нажмите Enter.")
+  }
+};
+
+game.init();
